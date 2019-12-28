ca0888cdda044f6cce14054ac3fe6213a12f2896
kunal444 <35965939+kunal444@users.noreply.github.com>
2019-12-28 18:12:51 +0530

    saved project

---
+++ InterviewApplication/InterviewApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Infoway Interview Application!");
+            try
+            {
+                Student obj = new Student();
+                obj.Selected += new InterviewDelegate(OracleDB);
+                obj.Selected += MySqlDB;
+                obj.Rejected += SqlServer;
+                obj.StudentId = 3489;
+                obj.StudentName = "Pravinkumar R. D.";
+                obj.City = "Pune";
+                obj.Phone = "+91 23893829";
+                obj.TotalMarks = 99;
+                if (obj.TotalMarks<=95)
+                {
+                    obj.Selected -= MySqlDB;
+                }
+                Console.WriteLine("Student Id is {0}", obj.StudentId);
+                bool result=false;
+                Console.WriteLine("Your result is {0}", obj.CalculateResult(obj.TotalMarks,ref result));
+                Console.WriteLine(result)
+;            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception - {0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+        private static void OracleDB()
+        {
+            Console.WriteLine("Student record stored in Oracle DB!");
+        }
+        private static void MySqlDB()
+        {
+            Console.WriteLine("Student record stored in MySql DB!");
+        }
+        private static void SqlServer()
+        {
+            Console.WriteLine("Student record stored in Sql Server DB!");
+        }
+    }
+}
---
+++ InterviewApplication/InterviewApplication/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewApplication
+{
+    public delegate void InterviewDelegate();
+    class Student
+    {
+        private int m_StudentId, m_TotalMarks;
+        public event InterviewDelegate Selected;
+        public event InterviewDelegate Rejected;
+        public int StudentId
+        {
+            get { return m_StudentId; }
+            set
+            {
+                if (value > 0)
+                {
+                    m_StudentId = value;
+                }
+                else
+                {
+                    throw new Exception("Please enter valid student Id");
+                }
+
+            }
+        }
+
+        public int TotalMarks { get => m_TotalMarks; set => m_TotalMarks = value; }
+        //Auto Implemented Properties
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string StudentName { get; set; }
+
+        public string CalculateResult(int totalMarks,ref bool yes)
+        {
+            if (totalMarks>=70)
+            {
+                if (Selected!=null)
+                {
+                    Selected();
+                }
+                yes = true;
+                return string.Format("Congrats! {0}! You have been selected for next round!", StudentName);
+            }
+            else
+            {
+                if (Rejected!=null)
+                {
+                    Rejected();
+                }
+                return string.Format("Sorry! {0}! You have been rejected for next round!", StudentName);
+            }
+        }
+    }
+}
